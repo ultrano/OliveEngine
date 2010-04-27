dbb0e194cfcc431c64144a5e6494fa5aa9ef26a9
ultrano <hahasasa@nate.com>
2010-04-27 23:14:42 +0900

    pimple 패턴 제거 1번째 OvObject에서 pimple패턴 제거

--- OliveG/OliveGLib/include/OvObject.h
+++ OliveG/OliveGLib/include/OvObject.h
@@ -27,9 +27,7 @@ public:
 
 private:
 
-	//! Member Values (Ensconsed for encapsulation)
-	//! Application can access member only using get,set interface
-	struct OvPimple;
-	OvAutoPtr<OvPimple> m_pPimple;
-	class OvProp_Pimple;
+	string			m_strObjectName;
+	OvObjectID		m_idObjectID;
+
 };
--- OliveG/OliveGLib/source/OvObject.cpp
+++ OliveG/OliveGLib/source/OvObject.cpp
@@ -7,71 +7,30 @@ using namespace std;
 
 OvRTTI_IMPL_PROP(OvObject,OvRefBase);
 OvPROP_BAG_IMPL(OvObject);
-struct OvObject::OvPimple : OvMemObject
-{
-	// 멤버변수들을 기술한다.
-	// 멤버변수또한 되도록 캡슐화 하기 위해 핌플패턴을 적용한다.
-
-	string			mObjectName;
-	const char*		mObjectDescription;
-	OvObjectID			m_hObjectID;
-
-	//! 로딩시에 쓰이게될, 세이브시에 저장되었던 예전 핸들번호.
-	//! 계층구조를 구축하는데에 표본으로 쓰인다.
-	//! 어플리케이션에선 쓰일 일이 없으므로 외부로 누출시키는 인터페이스를 제공하지 않는다.
-	OvObjectID			m_hOldHandle;
-
-};
-
-class OvObject::OvProp_Pimple : public OvProperty
-{
-	virtual bool	Store(OvObject* pObj, TiXmlElement& rXml)
-	{
-		OvAutoPtr<OvPimple>& krProp = *(OvAutoPtr<OvPimple>*)Access(pObj);
-		string strName = krProp->mObjectName;
-		return false;
-	};
-	virtual bool	Restore(OvObject* pObj, TiXmlElement& rXml){return false;};
-};
 
 void OvObject::RegisterProperties()
 {
-	{
-		OvProp_Pimple* kpPropProtector = new OvProp_Pimple;
-		kpPropProtector->SetOffset(offsetof(__this_class,m_pPimple));
-		kpPropProtector->SetPropertyName("Protected");
-		GetPropertyBag()->RegisterProperty(kpPropProtector);
-	}
 };
 
-// OvObject::OvObject(ovloader ploader):m_pPimple(new OvObject::OvPimple)
-// {
-// 	m_pPimple->m_hObjectID = OvObjectManager::Instance()->AllocObjectID(this);
-// 	ovstore kstore = ploader->getstore<OvObject>();
-// 	kstore.get
-// 
-// }
-
-OvObject::OvObject():m_pPimple(OvNew OvObject::OvPimple)
+OvObject::OvObject()
 {
-	m_pPimple->m_hObjectID = OvObjectManager::GetInstance()->AllocObjectID(this);
+	m_idObjectID = OvObjectManager::GetInstance()->AllocObjectID(this);
 }
 OvObject::~OvObject()
 {
 	OvObjectManager::GetInstance()->RecallObjectID(this);
-	m_pPimple = NULL;
 }
 
 void	OvObject::SetName(const char* _pName)
 {
-	m_pPimple->mObjectName = _pName;
+	m_strObjectName = _pName;
 }
 const char* OvObject::GetName()
 {
-	return m_pPimple->mObjectName.data();
+	return m_strObjectName.data();
 }
 
 const OvObjectID		OvObject::GetObjectID()
 {
-	return m_pPimple->m_hObjectID;
+	return m_idObjectID;
 }
