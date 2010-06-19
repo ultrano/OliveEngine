0eb07f873f6d02e09efee15d7de611af98b65cab
hahasasa <hahasasa@nate.com>
2010-06-19 16:53:02 +0900

    OvObjectID 과련 -코어 ID 타입을 Typedef로 인터페이스를 통일 시켰다 -코어타입을 받는 복사 생성자를 다시 열었다. (원래는 매니저에서만 접근 가능했었다.) -비교 연산자 != 를 추가 했다.

--- OliveG/OliveGLib/include/OvObjectID.h
+++ OliveG/OliveGLib/include/OvObjectID.h
@@ -4,22 +4,28 @@
 
 class OvObjectID
 {
-	friend class OvObjectManager;
-	OvObjectID( unsigned int );
-	unsigned int core;
 public:
+
+	typedef unsigned int id_core_type;
 
 	static OvObjectID	INVALID;
 
 	OvObjectID();
 	OvObjectID( OvObjectID& );
+	OvObjectID( id_core_type );
 	OvObjectID( const OvObjectID& );
 
 	friend bool operator==(const OvObjectID& idRef0, const OvObjectID& idRef1);
 	friend bool operator==( OvObjectID& idRef0,  OvObjectID& idRef1);
 
+	friend bool operator!=(const OvObjectID& idRef0, const OvObjectID& idRef1);
+	friend bool operator!=( OvObjectID& idRef0,  OvObjectID& idRef1);
+
 	bool operator==(const OvObjectID& idRef);
 	bool operator==(OvObjectID& idRef);
+
+	bool operator!=(const OvObjectID& idRef);
+	bool operator!=(OvObjectID& idRef);
 
 	friend bool operator<(const OvObjectID& idRef0, const OvObjectID& idRef1);
 	bool operator<(const OvObjectID& idRef);
@@ -28,4 +34,9 @@ public:
 	friend bool operator>(const OvObjectID& idRef0, const OvObjectID& idRef1);
 	bool operator>(const OvObjectID& idRef);
 	bool operator>(OvObjectID& idRef);
+
+private:
+
+	friend class OvObjectManager;
+	id_core_type core;
 };
--- OliveG/OliveGLib/source/OvObjectID.cpp
+++ OliveG/OliveGLib/source/OvObjectID.cpp
@@ -2,7 +2,7 @@
 
 OvObjectID	OvObjectID::INVALID(0);
 
-OvObjectID::OvObjectID(unsigned int copyid)
+OvObjectID::OvObjectID( id_core_type copyid)
 :core(copyid)
 {
 }
@@ -25,6 +25,9 @@ OvObjectID::OvObjectID( const OvObjectID& copyid)
 bool OvObjectID::operator==(const OvObjectID& idRef){return core == idRef.core;};
 bool OvObjectID::operator==(OvObjectID& idRef){return core == idRef.core;};
 
+bool OvObjectID::operator!=(const OvObjectID& idRef){return core != idRef.core;};
+bool OvObjectID::operator!=(OvObjectID& idRef){return core != idRef.core;};
+
 bool OvObjectID::operator<(const OvObjectID& idRef){return core < idRef.core;};
 bool OvObjectID::operator<(OvObjectID& idRef){return core < idRef.core;};
 
@@ -33,6 +36,9 @@ bool OvObjectID::operator>(OvObjectID& idRef){return core > idRef.core;};
 
 bool operator==(const OvObjectID& idRef0, const OvObjectID& idRef1){return idRef0.core == idRef1.core;};
 bool operator==( OvObjectID& idRef0,  OvObjectID& idRef1){return idRef0.core == idRef1.core;};
+
+bool operator!=(const OvObjectID& idRef0, const OvObjectID& idRef1){return idRef0.core != idRef1.core;};
+bool operator!=( OvObjectID& idRef0,  OvObjectID& idRef1){return idRef0.core != idRef1.core;};
 
 bool operator<(const OvObjectID& idRef0, const OvObjectID& idRef1){return idRef0.core < idRef1.core;};
 
