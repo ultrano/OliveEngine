1f507aca639816dadfb1914f4998d9dac841ae2c
hahasasa <hahasasa@nate.com>
2010-06-19 16:50:14 +0900

    OliveValue 관련 Value종류 추가.

--- OliveG/OliveGLib/include/OliveValue.h
+++ OliveG/OliveGLib/include/OliveValue.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "OvMemObject.h"
 #include "OvUtility_RTTI.h"
+#include "OvPoint2.h"
+#include "OvPoint3.h"
+#include "OvQuaternion.h"
+#include "OvObjectID.h"
 
 namespace OliveValue
 {
 
+#define	ACCEPT_STRING_CONSTRUCTOR	public:__this_class(){};__this_class(const string& strValue){SetValue(strValue);};
 	class Value : public OvMemObject
 	{
 		friend class OvObject;
@@ -20,8 +25,8 @@ namespace OliveValue
 	class Float : public OliveValue::Value
 	{
 		OvRTTI_DECL(Float);
+		ACCEPT_STRING_CONSTRUCTOR
 	public:
-		Float(){};
 		virtual void	SetValue( const string& expData );
 		virtual string	GetValue();
 	public:
@@ -30,12 +35,57 @@ namespace OliveValue
 	private:
 		float	m_value;
 	};
+	
+	class Point2 : public OliveValue::Value
+	{
+		OvRTTI_DECL(Point2);
+		ACCEPT_STRING_CONSTRUCTOR
+	public:
+		virtual void	SetValue( const string& expData );
+		virtual string	GetValue();
+	public:
+		void			SetPoint2( const OvPoint2& expValue );
+		void			SetPoint2( float x, float y );
+		const OvPoint2&	GetPoint2();
+	private:
+		OvPoint2	m_value;
+	};
+
+	class Point3 : public OliveValue::Value
+	{
+		OvRTTI_DECL(Point3);
+		ACCEPT_STRING_CONSTRUCTOR
+	public:
+		virtual void	SetValue( const string& expData );
+		virtual string	GetValue();
+	public:
+		void			SetPoint3( const OvPoint3& expValue );
+		void			SetPoint3( float x, float y, float z );
+		const OvPoint3&	GetPoint3();
+	private:
+		OvPoint3	m_value;
+	};
+
+	class Quaternion : public OliveValue::Value
+	{
+		OvRTTI_DECL(Quaternion);
+		ACCEPT_STRING_CONSTRUCTOR
+	public:
+		virtual void	SetValue( const string& expData );
+		virtual string	GetValue();
+	public:
+		void			  SetQuaternion( const OvQuaternion& expValue );
+		void			  SetQuaternion( float x, float y, float z, float w );
+		const OvQuaternion& GetQuaternion();
+	private:
+		OvQuaternion	m_value;
+	};
 
 	class Integer : public OliveValue::Value
 	{
 		OvRTTI_DECL(Integer);
+		ACCEPT_STRING_CONSTRUCTOR
 	public:
-		Integer(){};
 		virtual void	SetValue( const string& expData );
 		virtual string	GetValue();
 	public:
@@ -48,8 +98,8 @@ namespace OliveValue
 	class String : public OliveValue::Value
 	{
 		OvRTTI_DECL(String);
+		ACCEPT_STRING_CONSTRUCTOR
 	public:
-		String(){};
 		virtual void	SetValue( const string& expData );
 		virtual string	GetValue();
 	public:
@@ -57,6 +107,20 @@ namespace OliveValue
 		string			GetString();
 	private:
 		string		m_value;
+	};
+
+	class ObjectID : public OliveValue::Value
+	{
+		OvRTTI_DECL(ObjectID);
+		ACCEPT_STRING_CONSTRUCTOR
+	public:
+		virtual void	SetValue( const string& expData );
+		virtual string	GetValue();
+	public:
+		void			SetObjectID( const OvObjectID& expValue );
+		const OvObjectID&	GetObjectID();
+	private:
+		OvObjectID		m_value;
 	};
 
 	Value*	ValueFactory(const std::string& valueType);
--- OliveG/OliveGLib/source/OliveValue.cpp
+++ OliveG/OliveGLib/source/OliveValue.cpp
@@ -1,16 +1,24 @@
 #include "OliveValue.h"
+#include "OvStringUtility.h"
 
 using namespace OliveValue;
 OvRTTI_IMPL_ROOT(Value);
 
 OvRTTI_IMPL(Float);
+OvRTTI_IMPL(Point2);
+OvRTTI_IMPL(Point3);
+OvRTTI_IMPL(Quaternion);
 OvRTTI_IMPL(Integer);
 OvRTTI_IMPL(String);
+OvRTTI_IMPL(ObjectID);
 
 
 //////////////////////////////////////////////////////////////////////////
 REGIST_VALUE_TYPE_BEGINE
 	REGIST_VALUE_TYPE( Float )
+	REGIST_VALUE_TYPE( Point2 )
+	REGIST_VALUE_TYPE( Point3 )
+	REGIST_VALUE_TYPE( Quaternion )
 	REGIST_VALUE_TYPE( Integer )
 	REGIST_VALUE_TYPE( String )
 REGIST_VALUE_TYPE_END
@@ -48,6 +56,78 @@ float			Float::GetFloat()
 //////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////
+void			Point2::SetValue( const string& expData )
+{
+	sscanf_s( expData.c_str(), "%f,%f", &m_value.x, &m_value.y );
+}
+string			Point2::GetValue()
+{
+	return string( OvFormatString( "%f,%f", m_value.x, m_value.y ) );
+}
+void			Point2::SetPoint2( const OvPoint2& expValue )
+{
+	m_value = expValue;
+}
+void			Point2::SetPoint2( float x, float y )
+{
+	SetPoint2( OvPoint2( x, y ) );
+}
+const OvPoint2&	Point2::GetPoint2()
+{
+	return m_value;
+}
+//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////
+void			Point3::SetValue( const string& expData )
+{
+	sscanf_s( expData.c_str(), "%f,%f,%f", &m_value.x, &m_value.y, &m_value.z );
+}
+string			Point3::GetValue()
+{
+	return string( OvFormatString( "%f,%f,%f", m_value.x, m_value.y, m_value.z ) );
+}
+void			Point3::SetPoint3( const OvPoint3& expValue )
+{
+	m_value = expValue;
+}
+void			Point3::SetPoint3( float x, float y, float z )
+{
+	SetPoint3( OvPoint3( x, y, z ) );
+}
+const OvPoint3&	Point3::GetPoint3()
+{
+	return m_value;
+}
+//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////
+void				Quaternion::SetValue( const string& expData )
+{
+	sscanf_s( expData.c_str(), "%f,%f,%f,%f", &m_value.x, &m_value.y, &m_value.z, &m_value.w );
+}
+string				Quaternion::GetValue()
+{
+	return string( OvFormatString( "%f,%f,%f,%f", m_value.x, m_value.y, m_value.z, m_value.w ) );
+}
+void				Quaternion::SetQuaternion( const OvQuaternion& expValue )
+{
+	m_value = expValue;
+}
+void				Quaternion::SetQuaternion( float x, float y, float z, float w )
+{
+	SetQuaternion( OvQuaternion( x, y, z, w ) );
+}
+const OvQuaternion&	Quaternion::GetQuaternion()
+{
+	return m_value;
+}
+//////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////
 void			Integer::SetValue( const string& expData )
 {
 	m_value = boost::lexical_cast<int>(expData);
@@ -80,6 +160,25 @@ void			String::SetString( const string& expValue )
 	m_value = expValue;
 }
 string				String::GetString()
+{
+	return m_value;
+}
+//////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////
+void			ObjectID::SetValue( const string& expData )
+{
+	m_value = OvObjectID( boost::lexical_cast< OvObjectID::id_core_type >( expData ) );
+}
+string			ObjectID::GetValue()
+{
+	return boost::lexical_cast< string >( *(( OvObjectID::id_core_type* )&m_value) );
+}
+void			ObjectID::SetObjectID( const OvObjectID& expValue )
+{
+	m_value = expValue;
+}
+const OvObjectID& ObjectID::GetObjectID()
 {
 	return m_value;
 }
