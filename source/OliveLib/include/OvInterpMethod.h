b91ccd94838e6fd5775e04be6953b964b23107a2
hahasasa <hahasasa@nate.com>
2010-08-09 23:45:46 +0900

    iterpolator 는 메소드를 따로 빼지 않고 가기로 결정.

--- source/OliveLib/include/OvInterpMethod.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class OvInterpolator;
-
-namespace OvInterpMethod
-{
-	typedef float(*MethodType)( OvInterpolator* interplator, float src, float dest );
-	float Linear( OvInterpolator* interplator, float src, float dest );
-}
