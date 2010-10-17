f9be5f9ff5305120d6dca2c38bde790e890f3738
hahasasa <hahasasa@nate.com>
2010-10-18 00:37:17 +0900

    시연을 위해 아직 정립되지 않은 그림자 랜더링 부분을 지웠다. 디퓨즈만 랜더링 되도록 수정

--- source/TestProj/mesh_case.h
+++ source/TestProj/mesh_case.h
@@ -34,18 +34,10 @@ private:
 protected:
 	OvRenderTarget m_renderTarget;
 	OvTextureSPtr m_diffuseScene;
-	OvTextureSPtr m_lightDepthScene;
-	OvTextureSPtr m_shadowProjectedScene;
-	OvTextureSPtr m_shadowAccumulatedScene;
-
 	
 	OvShaderCodeSPtr m_shader_code;
 
-
 	OvCameraSPtr m_mainCamera;
-	list<OvCameraSPtr> m_lights;
-	OvXComponentSPtr m_cameraControler;
-
 	OvObjectCollector m_loadedObjects;
 
 	bool m_exitFlag;
@@ -65,17 +57,10 @@ public:
 		m_mainCamera = m_loadedObjects.GetByName("Camera");
 		m_mainCamera->SetFOV( D3DX_PI/2.0f );
 
-		OvObjectCollector components;
-		m_mainCamera->GetComponents( components );
-		m_cameraControler = components.GetByAt(0);
-
 		//////////////////////////////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////
 		m_diffuseScene = CreateRenderTexture( 800, 600, 1, D3DFMT_A8B8G8R8 );
-		m_lightDepthScene = CreateRenderTexture( 512, 512, 1, D3DFMT_R32F );
-		m_shadowProjectedScene = CreateRenderTexture( 800, 600, 1, D3DFMT_A8B8G8R8 );
-		m_shadowAccumulatedScene = CreateRenderTexture( 800, 600, 1, D3DFMT_A8B8G8R8 );
 
 		//////////////////////////////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////
@@ -83,33 +68,15 @@ public:
 
 		m_shader_code = OvResourceManager::GetInstance()->LoadResource<OvShaderCode>( ResDirPath("shader/test.shacode") );
 
-		m_shader_code->CompileVertexShader( "v_spot_shadow", "vs_2_0" );
-		m_shader_code->CompilePixelShader( "p_spot_shadow", "ps_2_0" );
-
 		m_shader_code->CompileVertexShader( "rectV", "vs_2_0" );
 		m_shader_code->CompilePixelShader( "rectP", "ps_2_0" );
-
-		m_shader_code->CompileVertexShader( "depthV", "vs_2_0" );
-		m_shader_code->CompilePixelShader( "depthP", "ps_2_0" );
-
-		m_shader_code->CompileVertexShader( "accumulateV", "vs_2_0" );
-		m_shader_code->CompilePixelShader( "accumulateP", "ps_2_0" );
-
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
-		//////////////////////////////////////////////////////////////////////////
 	};
 	GL_ENV_TEAR_DOWN
 	{
 		m_mainCamera = NULL;
-		m_lights.clear();
-		m_cameraControler = NULL;
 		m_loadedObjects.Clear();
 
 		m_diffuseScene = NULL;
-		m_lightDepthScene = NULL;
-		m_shadowProjectedScene = NULL;
-		m_shadowAccumulatedScene = NULL;
 
 		m_shader_code = NULL;
 
@@ -125,19 +92,6 @@ public:
 			{
 				switch ( msg.wParam )
 				{
-				case VK_SPACE:
-					{
-							OvCameraSPtr light = OvNew OvCamera;
-							light->SetTranslate( m_mainCamera->GetTranslate() );
-							light->SetRotation( m_mainCamera->GetRotation() );
-							light->SetFOV( m_mainCamera->GetFOV() );
-							light->Update(0);
-							light->SetFarClip(800);
-							(OvNew testcomponent)->SetTarget( light );
-							m_loadedObjects.AddObject( light );
-							m_lights.push_back( light );
-					}
-					break;
 				case VK_ESCAPE:
 					if ( !m_exitFlag )
 					{
@@ -153,19 +107,6 @@ public:
 							copymodel->SetTranslate( (m_mainCamera->GetLocalLookDirection() * 5.0f) + m_mainCamera->GetTranslate() );
 							m_loadedObjects.AddObject( copymodel );
  						}
-
-					}
-					break;
-				case VK_END :
-					{
-						OvStorage store;
-						store.Save( ResDirPath("ovf/scene_test2.xml"), m_loadedObjects);
-					}
-					break;
-				case VK_HOME :
-					{
-						OvStorage store;
-						store.Load( ResDirPath("ovf/scene_test2.xml"), m_loadedObjects);
 					}
 					break;
 				}
@@ -237,118 +178,11 @@ public:
 		OvRenderer::GetInstance()->EndTarget();
 		m_renderTarget.UnlockRenderTarget();
 	}
-
-	void	RenderDepth( const OvMatrix& view_project, OvObjectCollector objectList )
-	{
-		OvShaderManager::GetInstance()->SetVSConst( OvVShaderConst::ViewProject, view_project );
-
-		OvRenderer::GetInstance()->SetVertexShader( m_shader_code->FindShader( "depthV", "vs_2_0" ) );
-		OvRenderer::GetInstance()->SetPixelShader( m_shader_code->FindShader( "depthP", "ps_2_0" ) );
-
-		OvRenderer::GetInstance()->ClearTarget();
-		OvRenderer::GetInstance()->BeginTarget();
-		for ( int i = 0 ; i < objectList.Count() ; ++i )
-		{
-			OvXObjectSPtr obj = objectList.GetByAt(i);
-			if (OvRTTI_Util::IsKindOf<OvModel>(obj))
-			{
-				OvModelSPtr model = obj;
-				model->RenderWithoutMaterial();
-			}
-		}
-
-
-		OvRenderer::GetInstance()->EndTarget();
-
-	}
-	void	RenderSpotLightDepth( OvCameraSPtr camera, OvCameraSPtr light,OvObjectCollector objectList )
-	{
-		OvMatrix light_project;
-		OvShaderManager::GetInstance()->GetVSConst(OvVShaderConst::LightProject,light_project);
-		m_renderTarget.LockRenderTarget( 0, m_lightDepthScene->GetSurface() );
-		
-		RenderDepth( light_project, objectList );
-
-		m_renderTarget.UnlockRenderTarget();
-
-	}
-	void	RenderShadowProjected( OvCameraSPtr camera, OvCameraSPtr light,OvObjectCollector objectList )
-	{
-		static OvMatrix bias = OvMatrix().Scale( 0.5f, -0.5f, 0 ) * OvMatrix().Translate( 0.5f, 0.5f, 0 );
-		OvMatrix light_project;
-		OvShaderManager::GetInstance()->GetVSConst(OvVShaderConst::LightProject,light_project);
-		OvMatrix view_project = camera->GetViewMatrix() * camera->GetProjectMatrix();
-		OvShaderManager::GetInstance()->SetVSConst( OvVShaderConst::ViewProject, view_project );
-
-		LPDIRECT3DDEVICE9 device = OvRenderer::GetInstance()->GetDevice();
-		device->SetSamplerState( 0, D3DSAMP_ADDRESSU, D3DTADDRESS_BORDER );
-		device->SetSamplerState( 0, D3DSAMP_ADDRESSV, D3DTADDRESS_BORDER );
-
-		OvRenderer::GetInstance()->SetVertexShader( m_shader_code->FindShader( "v_spot_shadow", "vs_2_0" ) );
-		OvRenderer::GetInstance()->SetPixelShader( m_shader_code->FindShader( "p_spot_shadow", "ps_2_0" ) );
-		OvRenderer::GetInstance()->SetTexture( 0, m_lightDepthScene );
-
-		m_renderTarget.LockRenderTarget( 0, m_shadowProjectedScene->GetSurface() );
-
-		OvRenderer::GetInstance()->ClearTarget();
-		OvRenderer::GetInstance()->BeginTarget();
-		for ( int i = 0 ; i < objectList.Count() ; ++i )
-		{
-			OvXObjectSPtr obj = objectList.GetByAt(i);
-			if (OvRTTI_Util::IsKindOf<OvModel>(obj))
-			{
-				OvModelSPtr model = obj;
-
-				OvMatrix world_light_project = model->GetWorldMatrix() * light_project;
-				OvShaderManager::GetInstance()->SetVSConst( OvVShaderConst::WorldLightProject, world_light_project );
-				OvShaderManager::GetInstance()->SetVSConst( OvVShaderConst::WorldLightProjectBias, world_light_project * bias );
-				model->RenderWithoutMaterial();
-			}
-		}
-		OvRenderer::GetInstance()->EndTarget();
-		m_renderTarget.UnlockRenderTarget();
-	}
-	void	RenderShadowAccumulated()
-	{
-		OvRenderer::GetInstance()->SetTexture( 0, m_shadowProjectedScene );
-
-		LPDIRECT3DDEVICE9 device = OvRenderer::GetInstance()->GetDevice();
-		device->SetRenderState( D3DRS_ALPHABLENDENABLE, true);
-		device->SetRenderState( D3DRS_BLENDOP, D3DBLENDOP_ADD );
-		device->SetRenderState( D3DRS_SRCBLEND, D3DBLEND_ONE );
-		device->SetRenderState( D3DRS_DESTBLEND, D3DBLEND_ONE );
-
-		m_renderTarget.LockRenderTarget( 0, m_shadowAccumulatedScene->GetSurface() );
-
-		OvRenderer::GetInstance()->BeginTarget();
-
-		OvRenderer::GetInstance()->RenderUnitRect
-			( m_shader_code->FindShader( "accumulateV", "vs_2_0" )
-			, m_shader_code->FindShader( "accumulateP", "ps_2_0" ) );
-
-		OvRenderer::GetInstance()->EndTarget();
-
-		m_renderTarget.UnlockRenderTarget();
-		device->SetRenderState( D3DRS_ALPHABLENDENABLE, false);
-
-	}
 	void	Render( OvCameraSPtr camera, OvObjectCollector objectList )
 	{
 		RenderDiffuse( camera, objectList );
-		m_renderTarget.LockRenderTarget( 0, m_shadowAccumulatedScene->GetSurface() );
-		OvRenderer::GetInstance()->ClearTarget();
-		m_renderTarget.UnlockRenderTarget();
-		for each ( OvCameraSPtr light in m_lights )
-		{
-			OvMatrix light_project = light->GetViewMatrix() * light->GetProjectMatrix();
-			OvShaderManager::GetInstance()->SetVSConst( OvVShaderConst::LightProject, light_project );
-			RenderSpotLightDepth( camera, light, objectList );
-			RenderShadowProjected( camera, light, objectList );
-			RenderShadowAccumulated();
-		}
 
 		OvRenderer::GetInstance()->SetTexture( 0, m_diffuseScene );
-		OvRenderer::GetInstance()->SetTexture( 1, m_shadowAccumulatedScene );
 
 		OvRenderer::GetInstance()->ClearTarget();
 		OvRenderer::GetInstance()->BeginTarget();
