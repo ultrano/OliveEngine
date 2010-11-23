5273638a56e5d29d4840d6eb140543a0f2f3ab0c
hahasasa <hahasasa@nate.com>
2010-11-23 23:52:28 +0900

    현재 씬 개념 구축중.

--- source/OliveLib/include/OvScene.h
+++ source/OliveLib/include/OvScene.h
@@ -1,13 +1,64 @@
 #pragma once
 
 #include "OvXNode.h"
+#include "OvObjectCollector.h"
+#include <map>
+
+OvREF_POINTER(OvModel);
+OvREF_POINTER(OvCamera);
 
 class OvScene : public OvXNode
 {
 	OvRTTI_DECL( OvScene );
+	
+	typedef std::map< const OvRTTI *, OvObjectCollector > object_table;
+
+public: //!< Create Method
+
+	OvModelSPtr		CreateModel();
+	OvCameraSPtr	CreateCamera();
+
 public:
+
+	void			SetViewCamera( OvCameraSPtr camera );
+	OvCameraSPtr	GetViewCamera();
+
+public: //!< Find Method
+
+	OvModelSPtr		FindModel( const OvObjectID & objectID );
+	OvModelSPtr		FindModel( const std::string & objectName );
+
+	OvCameraSPtr	FindCamera( const OvObjectID & objectID );
+	OvCameraSPtr	FindCamera( const std::string & objectName );
+
+	template<typename T>
+	OvSmartPointer<T> FindObject( const OvObjectID & objectID );
+	template<typename T>
+	OvSmartPointer<T> FindObject( const std::string & objectName );
 
 private:
 
+	OvObjectSPtr	_find_object( const OvRTTI * rtti, const OvObjectID & objectID );
+	OvObjectSPtr	_find_object( const OvRTTI * rtti, const std::string & objectName );
+
+	OvObjectSPtr	_set_and_return( OvObjectSPtr object );
+
+private:
+
+	object_table	m_object_table;
+
+	OvCameraSPtr	m_view_camera;
+
 	//OvObjectCollector	m_
 };
+
+template<typename T>
+OvSmartPointer<T> OvScene::FindObject( const OvObjectID & objectID )
+{
+	return _find_object( T::GetRTTI(), objectID );
+}
+template<typename T>
+OvSmartPointer<T> OvScene::FindObject( const std::string & objectName )
+{
+	return _find_object( T::GetRTTI(), objectName );
+}
