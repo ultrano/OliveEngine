5ca75e773db202a2b8e3a1f918f9e8716aa74eae
unknown <hahasasa@.(none)>
2011-03-15 00:44:37 +0900

    소켓 작업중.

--- source/OliveLib/include/OvSocket.h
+++ source/OliveLib/include/OvSocket.h
@@ -1,8 +1,22 @@
 #pragma once
 #include "OvRefObject.h"
 
+#ifdef _WINSOCK2API_
+#include <winsock2.h>
+#include <Windows.h>
+#endif
+
+OvREF_POINTER(OvSocket);
 class OvSocket : public OvRefObject
 {
 	OvRTTI_DECL( OvSocket );
+private:
+	OvSocket();
+	~OvSocket();
+public:
 
+	OvSocketSPtr Connect( const string& ip, OvUInt port );
+
+private:
+	SOCKET m_socket;
 };
--- source/OliveLib/source/OvSocket.cpp
+++ source/OliveLib/source/OvSocket.cpp
@@ -1,2 +1,19 @@
 #include "OvSocket.h"
+
+//#include <winsock2.h>
 OvRTTI_IMPL( OvSocket );
+
+OvSocketSPtr OvSocket::Connect( const string& ip, OvUInt port )
+{
+	OvSocketSPtr ovsocket = OvNew OvSocket;
+	SOCKET sock = socket( AF_INET, SOCK_STREAM, 0 );
+	SOCKADDR_IN add_in;
+	add_in.sin_family = AF_INET;
+	add_in.sin_port = htons( port );
+	add_in.sin_addr.s_addr = inet_addr( ip.c_str() );
+
+	connect( sock, (SOCKADDR*)&add_in, sizeof(add_in) );
+
+	ovsocket->m_socket = sock;
+	return ovsocket;
+}
